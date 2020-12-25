ea02da6a54958ce6db171cf39d5915babb4890da
Michael Knepprath <mknepprath@gmail.com>
2020-12-24 21:35:18 -0500

    Bump it EVEN HIGHER

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 const BASE_URL =
   process.env.NODE_ENV === "development"
@@ -15,7 +15,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       // This is the reason this file was added - to set the `lang` attribute.
-      <html lang="en">
+      <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.png" />
           <meta name="twitter:card" content="summary_large_image" />
@@ -32,7 +32,7 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
